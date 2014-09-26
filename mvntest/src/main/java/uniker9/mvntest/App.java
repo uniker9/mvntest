f30857c41a7bb155c2874dedaef28269c0bee071
uniker9 <uniker9@gmail.com>
2014-09-26 17:57:16 +0900

    Add a sample code.

---
+++ mvntest/src/main/java/uniker9/mvntest/App.java
@@ -0,0 +1,13 @@
+package uniker9.mvntest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
